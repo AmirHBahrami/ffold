6019d443e7b5a5eabf75fb11d6da34fbe4027dd4
AmirHBahrami <amirhesam.bahrami@yahoo.com>
2022-06-06 04:44:29 +0200

    makeJar corrected

--- makeJar.sh
+++ makeJar.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-jar -cf ./deployed/ffold.jar bin/*
+./clean
+./compile
+mkdir deployed
+#touch deployed/MANIFEST.MF
+#echo "Main-Class: Main.class" >> deployed/MANIFEST.MF
+cd bin
+jar cfe ../deployed/ffold.jar Main Main.class
